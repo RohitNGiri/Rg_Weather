151bcba3c4e9946a24ee8687d67ebd91ec38994b
RohitNGiri <109196772+RohitNGiri@users.noreply.github.com>
2022-10-02 14:40:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const key = 'wBg3TVVETtPWWAZbk1viswDaeoEVb3ly';
+
+//get wheather information
+
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+}
+
+
+
+
+//get city information
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
+
+// getCity('manchester').then(data => {
+//     return getWheather(data.Key);
+// }).then(data => {
+//     console.log(data);
+// }).catch(err => console.log(err));
+
+
+
+//getWheather("329260");
